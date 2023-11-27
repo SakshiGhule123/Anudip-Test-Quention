d4607ae2db59c5a9d0615f580a423d01bd882b1a
SakshiGhule123 <132765520+SakshiGhule123@users.noreply.github.com>
2023-11-27 17:52:54 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+package com.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+
+		ArrayList list = new ArrayList();
+	}
+
+}
